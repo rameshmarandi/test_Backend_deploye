ac2917afe41be962d396f63129ea6331c7179066
Ramesh Marandi <57364523+rameshmarandi@users.noreply.github.com>
2024-10-05 00:31:06 +0530

    intial

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+
+const app = express();
+
+app.get("/", (req, res) => {
+     return res.json({message: "Hello World_depeinglid"});
+});
+
+
+
+app.listen(()=>{
+    console.log("Listening on port 3000");
+});
