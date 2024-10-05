af3b42c47336cac7e93754b3d69ac8ded2a9cc04
Ramesh Marandi <57364523+rameshmarandi@users.noreply.github.com>
2024-10-05 15:40:54 +0530

    chagnes port

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-      res.json({message: "Hello World_depeinglid"});
+app.post("/", (req, res) => {
+     return res.json({message: "Hello World_depeinglid"});
 });
 
 
 
-app.listen(()=>{
+app.listen(3000,()=>{
     console.log("Listening on port 3000");
 });
